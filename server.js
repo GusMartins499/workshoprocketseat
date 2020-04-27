44c4d1cbee489fcd78f342d4c6994db05311da9d
Gustavo Martins <gustavosm994@gmail.com>
2020-04-27 17:58:00 -0300

    Quase finalizado - vídeo 4 parte 2

--- server.js
+++ server.js
@@ -1,17 +1,63 @@
 const express = require('express')
 const server = express();
 
+const ideas = [
+    {
+        img: "https://image.flaticon.com/icons/svg/2729/2729007.svg",
+        title: "Cursos de Programação",
+        category: "Estudo",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+        url: "http://rocketseat.com.br"
+    },
+    {
+        img: "https://image.flaticon.com/icons/svg/2729/2729005.svg",
+        title: "Exercícios",
+        category: "Saúde",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+        url: "http://rocketseat.com.br"
+    },
+    {
+        img: "https://image.flaticon.com/icons/svg/2729/2729027.svg",
+        title: "Meditação",
+        category: "Mentalidade",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+        url: "http://rocketseat.com.br"
+    },
+    {
+        img: "https://image.flaticon.com/icons/svg/2729/2729032.svg",
+        title: "karaoke",
+        category: "Diversão em Família",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+        url: "http://rocketseat.com.br"
+    },
+]
+
 //configurar arquivos estáticos
 server.use(express.static("public"))
 
+//configuração nunjucks
+const nunjucks = require('nunjucks')
+nunjucks.configure("views", {
+    express: server,
+    noCache: true,
+})
+
 server.get("/", (req, res) => {
-    //return res.json({ message: 'Hello RocketSeat !' })
-    return res.sendFile(__dirname + "/index.html")
+    const reversedIdeas = [...ideas].reverse()
+    let lastIdeas = []
+    for (idea of reversedIdeas) {
+        if (lastIdeas.length < 2) {
+            lastIdeas.push(idea)
+        }
+    }
+
+    return res.render("index.html", { ideas: lastIdeas })
 })
 
 server.get("/ideias", (req, res) => {
-    //return res.json({ message: 'Hello RocketSeat !' })
-    return res.sendFile(__dirname + "/ideias.html")
+    const reversedIdeas = [...ideas].reverse()
+
+    return res.render("ideias.html", { ideas: reversedIdeas })
 })
 
 server.listen(3333);
